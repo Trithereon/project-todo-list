3e5d2e6a6ac851138844e26b360ce96780b11f38
Trithereon <eliottrassa@gmail.com>
2025-06-26 22:35:37 -0400

    Adjust main content styles and create class for task creation

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
+import Task from './create-task';
 import './styles.css';
 import 'modern-normalize/modern-normalize.css';
+
+const test = new Task(
+    'Get your shit together', 
+    'Go get all your shit from everywhere you left it and bring it all together',
+    1,
+    '23 April 2026'
+);
+console.log(test);
+
